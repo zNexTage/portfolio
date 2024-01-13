3884334ed7460bb5214adb2564fa3ea6fb7c405b
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-01-13 09:15:38 -0300

    Created projects section

---
+++ swiper.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 1,
+    autoplay: false,
+    loop: true,
+    effect: 'criative',
+    followFinger:true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
